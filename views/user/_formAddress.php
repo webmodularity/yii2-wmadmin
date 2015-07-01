4b7edf8b72a3d09eac0636ed176d007adf92130f
Rory Hellyer <rory@webmodularity.com>
2015-07-01 06:41:24 +0000

    street1 twice typo

--- views/user/_formAddress.php
+++ views/user/_formAddress.php
@@ -1,7 +1,7 @@
 <fieldset>
     <div class="row">
         <?= $form->field($model, 'street1')->placeholder("Street Address")->colSpan(['xs' => 6, 'sm' => 7, 'md' => 12, 'lg' => 7]) ?>
-        <?= $form->field($model, 'street1')->placeholder("Address Line 2 (Optional)")->colSpan(['xs' => 6, 'sm' => 5, 'md' => 12, 'lg' => 5]) ?>
+        <?= $form->field($model, 'street2')->placeholder("Address Line 2 (Optional)")->colSpan(['xs' => 6, 'sm' => 5, 'md' => 12, 'lg' => 5]) ?>
     </div>
     <div class="row">
         <?= $form->field($model, 'city')->placeholder('City')->colSpan(['xs' => 12, 'sm' => 5]) ?>
